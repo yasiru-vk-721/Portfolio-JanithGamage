f0c1e73e55bccc20e025e419949c1f23f29cfa60
Kaveesha.me <136825573+kavisha-jay@users.noreply.github.com>
2024-11-27 22:04:02 +0530

    Delete Frontend directory

--- Frontend/src/component/Aboutme.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Aboutme() {
-  return (
-    <div>Aboutme</div>
-  )
-}
-
-export default Aboutme
--- Frontend/src/component/Chemistry.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Chemistry() {
-  return (
-    <div>Chemistry</div>
-  )
-}
-
-export default Chemistry
